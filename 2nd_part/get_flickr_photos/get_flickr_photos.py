13c2a599f1f1d6046356e4d0265afeb8d0a05c63
kyh <kkyyhh96@outlook.com>
2017-01-16 15:37:43 +0800

    1-16first_commit

--- 2nd_part/get_flickr_photos/get_flickr_photos.py
+++ 2nd_part/get_flickr_photos/get_flickr_photos.py
@@ -4,7 +4,8 @@
 
 import flickrapi
 import time
-import psycopg2
+#import psycopg2
+import requests
 
 #flickr照片类
 class flickr_photo(object):
@@ -16,35 +17,38 @@ class flickr_photo(object):
         self.radius = radius
         self.date = date
 
-    def insert_db():
-        print('1')
+    def insert_db(self,url):
+        print(url)
 
 #flickr api信息
-def flickrapi():
-	api_key = u'382e669299b2ea33fa2288fd7180326a'
-	api_secret = u'b556d443c16be15e'
-	flickr = flickrapi.FlickrAPI(api_key, api_secret,cache=True)
-	return flickr
+def flickrAPI():
+    api_key=u'382e669299b2ea33fa2288fd7180326a'
+    api_secret=u'b556d443c16be15e'
+    flickr = flickrapi.FlickrAPI(api_key, api_secret,cache=True)
+    return flickr
 
 #获取照片
 def get_photo_from_location(site,latitude,longitude,datemin,datemax,r=5):
-	flickr = flickrapi()
-	print('0')
-	try:
-		#获取所有图片
-		photos = flickr.walk(lat=latitude,lon=longitude,radius=r,
-					 min_taken_date=datemin,max_taken_date=datemax,per_page=500,extras='url_c')
-	except Exception as e:
-		print(e)
-	#获取每一张图片
-	try:
-		for photo_url in photos:
-			url = photo.get('url_c')
-			if url is not None:
-				photo = flickr_photo(url,site,latitude,longitude,r,datemin)
-				photo.insert_db()#插入数据库
-	except Exception as e:
-		print(e)
+    flickr = flickrAPI()
+    try:
+        #获取所有图片
+        photos = flickr.walk(lat=latitude,lon=longitude,radius=r,
+                             min_taken_date=datemin,max_taken_date=datemax,per_page=500,extras='url_c')
+    except Exception as e:
+        print(e)
+    #获取每一张图片
+    try:
+        for photo_url in photos:
+            url = photo_url.get('url_c')
 
-get_photo_from_location('5NiagaraFalls',43.0828,-79.0742,'2015-06-01','2015-06-30',1)
+            if url is not None:
+                print(photo_url.get('id') + '\t' + photo_url.get('owner'))
+                photo = flickr_photo(url,site,latitude,longitude,r,datemin)
+                photo.insert_db(url)#插入数据库
+    except Exception as e:
+        print(e)
+    test=flickr.photos.geo.getLocation(photo_id=17802805013,format='rest')
+    print(test)
+
+get_photo_from_location('5NiagaraFalls',43.0828,-79.0742,'2015-06-02','2015-06-03',1)
 
