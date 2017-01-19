ef9fb1ad37ee4ff59c81187a6e92806608d75c1c
kyh <kkyyhh96@163.com>
2017-01-19 14:43:26 +0800

    1-19first_commit

--- 2nd_part/2create_table.sql
+++ 2nd_part/2create_table.sql
@@ -4,8 +4,8 @@ site_name CHARACTER VARYING(30) NOT NULL,
 coordinates POINT NOT NULL,
 start_query BOOLEAN NOT NULL DEFAULT(FALSE),
 );
-CREATE INDEX site_name_index ON site_name;
-CREATE INDEX start_query ON site_name;
+CREATE INDEX site_name_index ON site(site_name);
+CREATE INDEX start_query ON site(start_query);
 
 CREATE TABLE photo
 (id INTEGER PRIMARY KEY,
@@ -21,13 +21,13 @@ start_detect BOOLEAN NOT NULL DEFAULT(FALSE),
 start_info BOOLEAN NOT NULL DEFAULT(FALSE),
 start_recog BOOLEAN NOT NULL DEFAULT(FALSE)
 );
-CREATE INDEX photo_id_index ON id;
-CREATE INDEX photo_coordinates_index ON coordinates;
-CREATE INDEX photo_date_index ON photo_date;
-CREATE INDEX photo_f_hasface_index ON f_hasface;
-CREATE INDEX photo_start_detect_index ON start_detect;
-CREATE INDEX photo_start_info_index ON start_info;
-CREATE INDEX photo_start_recog_index ON start_recog;
+CREATE INDEX photo_id_index ON photo(id);
+CREATE INDEX photo_coordinates_index ON photo USING GIST(coordinates);
+CREATE INDEX photo_date_index ON photo(photo_date);
+CREATE INDEX photo_f_hasface_index ON photo(f_hasface);
+CREATE INDEX photo_start_detect_index ON photo(start_detect);
+CREATE INDEX photo_start_info_index ON photo(start_info);
+CREATE INDEX photo_start_recog_index ON photo(start_recog);
 
 CREATE TABLE facepp
 (id INTEGER PRIMARY KEY,
@@ -39,7 +39,7 @@ race CHARACTER(5) NOT NULL,
 smiling FLOAT NOT NULL,
 glass FLOAT NOT NULL
 );
-CREATE INDEX facepp_photo_id_index ON photo_id;
+CREATE INDEX facepp_photo_id_index ON facepp(photo_id);
 
 CREATE TABLE ms_emotion
 (id INTEGER PRIMARY KEY,
@@ -54,4 +54,4 @@ neutral FLOAT NOT NULL,
 sadness FLOAT NOT NULL,
 surprise FLOAT NOT NULL
 );
-CREATE INDEX ms_emotion_photo_id_index ON photo_id;
+CREATE INDEX ms_emotion_photo_id_index ON ms_emotion(photo_id);
